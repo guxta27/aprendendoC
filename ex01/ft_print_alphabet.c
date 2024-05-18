9c6c39402e04dee1e115e34085e42c3854dbda81
guxta27 <gustavoromulobrum@gmail.com>
2024-05-18 18:06:34 -0300

    ft_print_alphabet

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
 
 void ft_print_alphabet(void){
-char letras;
-for(letras = 'a'; letras <= 'z'; letras++){
-write(1,&letras,1);
+
+ char letras;
+
+ for(letras = 'a'; letras <= 'z'; letras++){
+ write(1,&letras,1);
  }
 }
 
