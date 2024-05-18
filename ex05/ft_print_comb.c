9f5e96ce926f55d5d2f8e733035b21d221775303
guxta27 <gustavoromulobrum@gmail.com>
2024-05-18 20:44:07 -0300

    ft_print_comb

---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void ft_print_comb(void){
+ 
+  char a,b,c;
+ 
+
+  for(a = '0'; a <= '7'; a++){
+    for(b = a+1; b <= '8'; b ++){
+      for(c = b+1; c <= '9'; c ++){
+   	
+        write(1,&a,1);
+        write(1,&b,1);
+        write(1,&c,1);
+        write(1,", ",2);
+
+            }
+     
+        }
+   
+     }
+ 
+ }
